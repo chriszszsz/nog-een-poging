909bf10e3d722f9128bbe74ca7f5cbc3a00636d3
Christian Hulleman <chulleman32@gmail.com>
2026-06-09 17:27:55 +0200

    Add role based login redirects

--- schema.sql
+++ schema.sql
@@ -1,19 +1,2 @@
-INSERT INTO users
-(
-    company_id,
-    first_name,
-    last_name,
-    email,
-    password_hash,
-    role
-)
-
-VALUES
-(
-    1,
-    'Christian',
-    'Hulleman',
-    'christian@axians.nl',
-    '$2b$10$dIUDDqHLZAa3masQbDZzu.U48jkE5lmLNmReGV56CW7IEFQ24x3IS',
-    'admin'
-);
+SELECT *
+FROM users;
