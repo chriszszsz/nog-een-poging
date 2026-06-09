da77b861a6e4b462a089fa6e6829db5dbba60a04
Christian Hulleman <chulleman32@gmail.com>
2026-06-09 23:20:50 +0200

    Force enable CORS headers

--- src/index.js
+++ src/index.js
@@ -67,26 +67,39 @@ app.get(
    MIDDLEWARE
 ========================= */
 
-app.use(cors({
-  origin: "*",
-  methods: [
-    "GET",
-    "POST",
-    "PUT",
-    "DELETE",
-    "OPTIONS"
-  ],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization"
-  ]
-}));
+const app = express();
 
-app.options("*", cors());
+app.use((req, res, next) => {
+
+  res.header(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+
+});
+
+app.use(cors());
 
 app.use(helmet());
 
 app.use(express.json());
+
 
 /* =========================
    HEALTH CHECKS
