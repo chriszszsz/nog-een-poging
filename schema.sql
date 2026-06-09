dcea5b9a710b7bf3cd583b233c0c1b7f52fb605d
Christian Hulleman <chulleman32@gmail.com>
2026-06-09 18:04:33 +0200

    Add campaign modal and participant selection

--- schema.sql
+++ schema.sql
@@ -1,2 +1,17 @@
-SELECT *
-FROM users;
+INSERT INTO users
+(
+    company_id,
+    first_name,
+    last_name,
+    email,
+    password_hash,
+    role
+)
+
+VALUES
+
+(1, 'Lisa', 'Jansen', 'lisa@test.nl', '$2b$10$dIUDDqHLZAa3masQbDZzu.U48jkE5lmLNmReGV56CW7IEFQ24x3ISik', 'participant'),
+
+(1, 'Tom', 'Bakker', 'tom@test.nl', '$2b$10$dIUDDqHLZAa3masQbDZzu.U48jkE5lmLNmReGV56CW7IEFQ24x3ISik', 'participant'),
+
+(1, 'Emma', 'Visser', 'emma@test.nl', '$2b$10$dIUDDqHLZAa3masQbDZzu.U48jkE5lmLNmReGV56CW7IEFQ24x3ISik', 'participant');
