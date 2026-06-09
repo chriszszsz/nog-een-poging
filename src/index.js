32c3fe7081f06144a5db5625e074c7d406900f6f
Christian Hulleman <chulleman32@gmail.com>
2026-06-09 23:40:59 +0200

    Definitive Azure CORS fix

--- src/index.js
+++ src/index.js
@@ -67,32 +67,36 @@ app.get(
    MIDDLEWARE
 ========================= */
 
-app.use(cors());
-
 app.use((req, res, next) => {
 
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Origin",
     "*"
   );
 
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
 
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
   );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
 
   next();
 
 });
 
+app.use(express.json());
+
 app.use(helmet());
 
-app.use(express.json());
+app.use(cors());
 
 
 /* =========================
